e7ad9663c0e4285613aa498e9f6b3645b6da5a87
Soo Hwan Park <sopark@linkedin.com>
2018-03-16 20:50:00 -0400

    fix acceptance test

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -5,9 +5,9 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /list-rentals', async function(assert) {
-    await visit('/list-rentals');
+  test('visiting /', async function(assert) {
+    await visit('/');
 
-    assert.equal(currentURL(), '/list-rentals');
+    assert.equal(currentURL(), '/');
   });
 });
