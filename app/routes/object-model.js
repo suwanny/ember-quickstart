598bc969ff7c3e6ce3ffae486777614c2ad8267d
Soo Hwan Park <sopark@linkedin.com>
2018-04-02 01:33:41 -0700

    Object Model 2

--- app/routes/object-model.js
+++ app/routes/object-model.js
@@ -8,16 +8,49 @@ const FooObject = EmberObject.extend({
   })
 });
 
+const Person = EmberObject.extend({
+  firstName: null,
+  lastName: null,
+  age: null,
+  country: null,
+
+  fullName: computed("firstName", "lastName", {
+    get() {
+      return `${this.get('firstName')} ${this.get('lastName')}`;
+    },
+    set(key, value) {
+      let [firstName, lastName] = value.split(/\s+/);
+      this.set('firstName', firstName);
+      this.set('lastName',  lastName);
+      return value;
+    }
+  }),
+
+  description: computed("fullName", "age", "country", function() {
+    return `${this.get(
+      "fullName"
+    )}; Age: ${this.get("age")}; Country: ${this.get("country")}`;
+  })
+});
+
+
 export default Route.extend({
   model() {
     const obj = FooObject.create({
       baz: { foo: "BLAMMO", bar: "BLAZORZ" }
     });
 
+    let captainAmerica = Person.create({
+      firstName: 'Steve',
+      lastName: 'Rogers',
+      age: 80,
+      country: 'USA'
+    });
+
     setTimeout(() => {
       obj.set('baz.bar', 'Changed ====== New BAR .... ')
     }, 3000);
 
-    return { output: "This is initial output", obj };
+    return { output: "This is initial output", obj, captainAmerica };
   }
 });
